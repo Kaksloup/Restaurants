333824515e3f228ec3e8a3dec1c0444fde6cf1a0
Abdoulaye Kake <abdoulaye.kake@gamma-project.fr>
2024-07-28 16:20:45 +0200

    Action : Fix test

--- Tests/Restaurants.Tests.Api/BaseTestClassFixture.cs
+++ Tests/Restaurants.Tests.Api/BaseTestClassFixture.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Moq;
 using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Database;
 
 namespace Restaurants.Tests.Api;
 
@@ -14,6 +15,8 @@ public class BaseTestClassFixture : IClassFixture<WebApplicationFactory<Program>
     protected readonly HttpClient Client;
     protected readonly Mock<IRestaurantsRepository> RestaurantsRepositoryMock = new();
 
+    protected readonly Mock<IRestaurantsSeeder> RestaurantsSeederMock = new();
+
     public BaseTestClassFixture(WebApplicationFactory<Program> factory)
     {
         _factory = factory.WithWebHostBuilder(builder =>
@@ -21,8 +24,12 @@ public class BaseTestClassFixture : IClassFixture<WebApplicationFactory<Program>
             builder.ConfigureTestServices(services =>
             {
                 services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+
                 services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantsRepository),
                     _ => RestaurantsRepositoryMock.Object));
+
+                services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantsSeeder),
+                    _ => RestaurantsSeederMock.Object));
             });
         });
 
